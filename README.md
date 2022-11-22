0ae8b160439e7e9deba41cd0481a24bf3c4eb58f
mosidrum <mosiokanga@gmail.com>
2022-11-22 12:15:54 +0100

    everything is done

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# Sign Up Form 
+This project gets data(Firstname, Lastname, Email Address, and Password) from the user and diplays it 
+## Authors
+
+- [@mosidrum](https://www.github.com/mosidrum)
+
+
+## 🚀 About Me
+I'm am an aspiring frontend web developer...
+
+
+# Hi, I'm Mosidrum! 👋
+
+
+## Feedback
+
+If you have any feedback on ways i can improve accordingly, please reach out to us at mosiokanga@gmail.com
+
+
+## 🔗 Links
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/in/isaac-ayodele-37890521b)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/mosidrum)
+
+
+## Installation
+
+View my-project with a live server on vs code
+
+```bash
+  
+```
+    
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Tech Stack
+
+**Client:** HTML5, CSS3, JS
+
+**Server:** Live server
+
+
+## Lessons Learned
+Javascript is breath 
--- app.js
+++ app.js
@@ -11,8 +11,45 @@ const signUp = e =>{
     // setting an item and assigning formData object to the item
     localStorage.setItem('formData', JSON.stringify(formData));
     // to view the localStorage on console
-    console.log(localStorage.getItem('formData'));
+    // console.log(localStorage.getItem('formData'));
+    // function to display details
+    displayData();
     // preventing user from submitting an empty form
     e.preventDefault();
 
 }
+
+// using a function to display data
+function displayData(){
+    // console.log(JSON.parse(localStorage.getItem('formData')));
+    if(localStorage.getItem('formData')){
+    // destructuring
+        let {fname, lname, email, pwd} = JSON.parse(localStorage.getItem('formData'));
+        var output = document.getElementById('output');
+        output.innerHTML=`
+            <table>
+                <tbody>
+                    <tr>
+                        <td> First Name </td>
+                        <td>${fname}</td>
+                    </tr>
+                    <tr>
+                        <td> Last Name </td>
+                        <td>${lname}</td>
+                    </tr>
+                    <tr>
+                        <td> Email Address </td>
+                        <td>${email}</td>
+                    </tr>
+                    <tr>
+                        <td> Password </td>
+                        <td>${pwd}</td>
+                    </tr>
+                </tbody>
+            </table>
+        `
+
+    }
+}
+// to display details when the page loads
+displayData();
